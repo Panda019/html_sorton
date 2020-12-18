d1a0f4b274481f51d3dcb97334f3084039572ca7
Panda <panda.019019@gmail.com>
2020-12-18 11:17:43 +0500

    Initial commit

---
+++ img/dbconfig.php
@@ -0,0 +1,11 @@
+<?
+$db_server = "u313311.mysql.masterhost.ru";
+//$db_server = "localhost";
+
+$db_name = "u313311_old_sotd";
+$db_user = "u313311";
+$db_pass = "stremiatus2e";
+$db_prefix = "";
+define('PAGE_EXT','.html');
+define('DBG_MODE',0);
+?>
